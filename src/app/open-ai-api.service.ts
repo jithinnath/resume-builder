966a3b5b74cd0fc46876c43c5256db91a7b7336f
Jithin <jithin.nath@edgeverve.com>
2023-02-16 22:21:40 +0530

    changes for middleware deployed in railway app

--- src/app/open-ai-api.service.ts
+++ src/app/open-ai-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {apiKey} from './Openai_API_KEY'
+import { apiKey } from './Openai_API_KEY'
 
 @Injectable({
   providedIn: 'root'
@@ -8,40 +8,24 @@ import {apiKey} from './Openai_API_KEY'
 export class OpenAiApiService {
 
   private readonly apiKey = apiKey.key;
-private readonly apiUrl = 'https://api.openai.com/v1/completions';
+  private readonly apiUrl = `https://openai-api-production-155f.up.railway.app/api/complete`;
 
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  public rephrase = (sentence:string) =>{
-    // const prompt = `Hello chat gpt this is great to talk to you. i want to repharas this sentence as it would help me to do well`;
-    const promt = `rephrase this for resume employment experience: "${sentence}" `;
-    const headers = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-    .set('Authorization', `Bearer ${this.apiKey}`);
-      const body ={
-        "model": "text-davinci-003",
-        prompt: promt,//`Rephrase this sentence: "${sentence}"`,
-        temperature: 0.5,
-        max_tokens: 512
-
-      }
-
-      return this.http.post(this.apiUrl,body,{headers:headers});
+  public rephrase = (sentence: string) => {
+    return this.openAi(`rephrase this for resume employment experience: "${sentence}" `)
   }
 
-  public complete = (sentence:string) =>{
-    const headers = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-    .set('Authorization', `Bearer ${this.apiKey}`);
-      const body ={
-        "model": "text-davinci-003",
-        prompt: `Complete this sentence: "${sentence}"`,
-        temperature: 0.5,
-        max_tokens: 512
+  public complete = (sentence: string) => {
+    return this.openAi(`Complete this sentance for my resume: "${sentence}"`)
+  }
 
-      }
-
-      return this.http.post(this.apiUrl,body,{headers:headers});
+  public openAi(prompt: string) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    const body = {
+      prompt: prompt,
+    }
+    return this.http.post(this.apiUrl, body, { headers: headers });
   }
 }
